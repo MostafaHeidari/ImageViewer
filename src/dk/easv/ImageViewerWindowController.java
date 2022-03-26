5f21af67beac3a0a07fe3f6e3b21a6b18ae1183a
FrederikReigstad <Frederik9876@gmail.com>
2022-03-26 21:33:41 +0100

    Done SlideShow lavet og virker kan add nye bilder og den husker alle sammen + Start og Stop knap der viker og er en kanp der skifter text

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -3,18 +3,22 @@ package dk.easv;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ImageViewerWindowController
-{
+public class ImageViewerWindowController {
     private final List<Image> images = new ArrayList<>();
+    public Button stopBtn;
     private int currentImageIndex = 0;
 
     @FXML
@@ -23,30 +27,38 @@ public class ImageViewerWindowController
     @FXML
     private ImageView imageView;
 
+    private static int time = 2500;
+
+    int count; //declared as global variable
+    private Task task;
+    private Thread thread;
+    private List<File> files;
+
     @FXML
-    private void handleBtnLoadAction()
-    {
+    private void handleBtnLoadAction() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
         fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
                 "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));
-        List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
+        files = fileChooser.showOpenMultipleDialog(new Stage());
 
-        if (!files.isEmpty())
-        {
+
+        if (!files.isEmpty()) {
             files.forEach((File f) ->
             {
                 images.add(new Image(f.toURI().toString()));
             });
-            displayImage();
+        }
+        if(thread == null || !thread.isAlive()){
+            task = getTask();
+            thread = new Thread(task);
+            thread.start();
         }
     }
 
     @FXML
-    private void handleBtnPreviousAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnPreviousAction() {
+        if (!images.isEmpty()) {
             currentImageIndex =
                     (currentImageIndex - 1 + images.size()) % images.size();
             displayImage();
@@ -54,20 +66,56 @@ public class ImageViewerWindowController
     }
 
     @FXML
-    private void handleBtnNextAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnNextAction() {
+        if (!images.isEmpty()) {
             currentImageIndex = (currentImageIndex + 1) % images.size();
             displayImage();
         }
     }
 
-    private void displayImage()
-    {
-        if (!images.isEmpty())
-        {
+    private void displayImage() {
+        if (!images.isEmpty()) {
             imageView.setImage(images.get(currentImageIndex));
         }
     }
+
+    public void stopBtn(ActionEvent event) {
+        if (thread.isAlive()){
+            thread.interrupt();
+            stopBtn.setText("Start");
+        }
+        else {
+            task = getTask();
+            thread = new Thread(task);
+            thread.start();
+            stopBtn.setText("Stop");
+        }
+    }
+
+    private Task<Void> getTask(){
+        return new Task<Void>() {
+            @Override
+            public Void call() throws Exception {
+                for (int i = 0; count < images.size(); i++) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImage(images.get(count));
+                            count++;
+                            if (count >= images.size()) {
+                                count = 0;
+                            }
+                        }
+                    });
+
+                    Thread.sleep(time);
+                }
+                return null;
+            }
+        };
+    }
 }
+
+
+
+
